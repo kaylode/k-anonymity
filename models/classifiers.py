9dc2f9c91f3697b8fdd8adafef05c68b0860d690
kaylode <pmkhoi@selab.hcmus.edu.vn>
2021-07-06 23:26:33 +0700

    add replace generalization

--- models/classifiers.py
+++ models/classifiers.py
@@ -1,11 +1,15 @@
+import enum
 import pickle
-from sklearn import tree, svm
+import pandas as pd
+from tqdm import tqdm
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 
 class KNN:
     """
-    Support Vector Machine Classifier
+    K- Nearest Neighbors Classifier
     """
     def __init__(self, num_classes) -> None:
         # One vs Rest classifier
@@ -50,12 +54,16 @@ class SVM:
         self.model = pickle.load(open(path, 'rb'))
 
 
-class DecisionTree:
+class RFs:
     """
     Decision Tree Classifier
     """
-    def __init__(self) -> None:
-        self.model = tree.DecisionTreeClassifier()
+    def __init__(self, seed=0) -> None:
+        self.model = RandomForestClassifier(
+                        n_estimators=300, 
+                        oob_score=True,
+                        min_samples_split=5, 
+                        max_depth=10, random_state=seed)
         
     def fit(self, inputs, targets):
         self.model = self.model.fit(inputs, targets)
@@ -65,9 +73,6 @@ class DecisionTree:
             class_names = None
         preds = self.model.predict(inputs)
         return classification_report(targets, preds, target_names=class_names, output_dict=True)
-        
-    def plot_tree(self):    
-        tree.plot_tree(self.model)
 
     def save_model(self, path):
         pickle.dump(self.model, open(path, 'wb'))
@@ -75,33 +80,93 @@ class DecisionTree:
     def load_model(self, path):
         self.model = pickle.load(open(path, 'rb'))
 
-def generate_mapping(df):
+def one_hot_encoding(df):
     rows, columns = df.shape
-    mapping_dict = [{} for i in range(columns)]
-    mapping_count = [0 for i in range(columns)]
+    attributes = list(df.columns)
+    is_cat_list = []
+
+    # Get list of categorical attribute
     for idx, row in df.iterrows():
         for j, value in enumerate(row):
             try:
                 float(value)
+                is_cat = False
             except ValueError:
-                if value not in mapping_dict[j].keys():
-                    mapping_dict[j][value] = mapping_count[j]
-                    mapping_count[j] += 1
+                # Is categorical attribute
+                is_cat = True
+            is_cat_list.append(is_cat)
+        break
+
+    # Name of categorical attributes
+    cat_attrs = [attributes[idx] for idx,i in enumerate(is_cat_list) if i]
+
+    # One hot encoding all categorical attribtutes
+    df = pd.get_dummies(df, columns=cat_attrs)
     
-    return mapping_dict
+    return df
 
-def embedding(df, mapping_dict):
-    features = []
-    targets = []
-    for idx, row in df.iterrows():
-        item = [mapping_dict[i][value] if value in mapping_dict[i].keys() else value for i, value in enumerate(row)]
-        features.append(item[:-1])
-        targets.append(item[-1])
+def embed_target(targets):
+    unique_labels = set(targets)
+    label_to_idx = {v:i for i, v in enumerate(unique_labels)} 
+    new_targets = [label_to_idx[i] for i in targets]
+    return new_targets, label_to_idx
 
-    return features, targets
+def replace_generalization(anon_df, columns, qi_index=None, is_cat=None):
+    """
+    Replace all generalized value to its mean
+    """
+
+    def get_mean(value):
+        tmp = value.split('~')
+        if len(tmp) == 2:
+            low, high = tmp
+            mean = (float(high) - float(low))/2
+            return mean
+        else:
+            return float(value)
+
+    def get_caterogical_value(key, value):
+        value_splits = value.split('~')
+        return [key+'_'+i for i in value_splits]
+
+    tmp_list = []
+    qi_index = [i-1 for i in qi_index]
+    for _, row in tqdm(anon_df.iterrows()):
+        atr_dict = {
+            key:0 for key in columns
+        }
+        for atr_idx, key in enumerate(list(row.keys())):
+            value = row[atr_idx]
+            # If not QID, append value
+            if atr_idx not in qi_index:
+                atr_dict[key] = value
+                continue
+            else:
+                # If is QID
+                # If is categorical
+                qi_id = qi_index.index(atr_idx)
+                if is_cat[qi_id]:
+                    keys = get_caterogical_value(key, value)
+                    for new_key in keys:
+                        atr_dict[new_key] = 1
+                else:
+                    # If is numeric
+                    mean = get_mean(value)
+                    atr_dict[key] = mean
+        tmp_list.append(atr_dict)
+
+    result_dict = {
+        k:[] for k in columns
+    }
+
+    for item in tmp_list:
+        for atr in item.keys():
+            result_dict[atr].append(item[atr])
+    
+    new_df = pd.DataFrame(result_dict)
+    return new_df
 
 if __name__ == '__main__':
-    import pandas as pd
 
     with open("./data/adult/adult_train.txt", 'r') as f:
         data = f.read()
@@ -112,20 +177,62 @@ if __name__ == '__main__':
         val_indexes = [int(i) for i in data.splitlines()]
 
     df = pd.read_csv('./data/adult/adult.csv', delimiter=';')
-
-    df = df.drop(['ID'], axis=1)
-
-    train_df = df.loc[train_indexes]
-    val_df = df.loc[val_indexes]
-
-    mapping_dict = generate_mapping(df)
-
-    train_features, train_targets = embedding(train_df, mapping_dict)
-    val_features, val_targets = embedding(val_df, mapping_dict)
+    anon_df = pd.read_csv('./results/adult/classic_mondrian/anon_dataset/adult_anonymized_2_0.csv', delimiter=';')
     
-    dt = KNN(num_classes=2)
+    QI_INDEX = [1, 2, 3, 4, 5, 6, 7, 8]
+    IS_CAT = [True, False, True, True, True, True, True, True]
 
+    # Drop ID and Target columns (last column)
+    df = df.drop(['ID'], axis=1)
+    targets = list(df.iloc[:, -1])
+    df = df.drop(df.columns[-1], axis=1)
+
+    anon_df = anon_df.drop(['ID'], axis=1)
+    anon_df = anon_df.drop(anon_df.columns[-1], axis=1)
+
+    # One-hot categorical values
+    one_hot_df = one_hot_encoding(df)
+
+    print("Replacing all generalized values...")
+    one_hot_anon_df = replace_generalization(
+        anon_df, 
+        qi_index=QI_INDEX,
+        is_cat=IS_CAT,
+        columns=list(one_hot_df.columns))
+
+    # One-hot target labels
+    embeded_targets, label_to_idx = embed_target(targets)
+
+    dt = KNN(num_classes=len(label_to_idx))
+
+    # Train original dataset
+    # Split train/test dataset
+    train_features = one_hot_df.iloc[train_indexes]
+    train_targets = [embeded_targets[i] for i in train_indexes]
+    
+    val_features = one_hot_df.iloc[val_indexes]
+    val_targets = [embeded_targets[i] for i in val_indexes]
+    
+    # Fit
     dt.fit(train_features, train_targets)
-    result = dt.eval(val_features, val_targets, mapping_dict[-1].keys())
+
+    # Evaluation
+    result = dt.eval(val_features, val_targets, label_to_idx.keys())
+
+    print(result)
+
+    # Train anonymized dataset
+    # Split train/test dataset
+    train_features = one_hot_anon_df.iloc[train_indexes]
+    train_targets = [embeded_targets[i] for i in train_indexes]
+    
+    val_features = one_hot_anon_df.iloc[val_indexes]
+    val_targets = [embeded_targets[i] for i in val_indexes]
+    
+    # Fit
+    dt.fit(train_features, train_targets)
+
+    # Evaluation
+    result = dt.eval(val_features, val_targets, label_to_idx.keys())
 
     print(result)
